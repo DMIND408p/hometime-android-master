1b35f951c428613a2db20d4303c4156bd811520b
DMIND408P <yyog6@student.monash.edu>
2018-10-27 18:15:33 +1100

    Icon changed and add new test cases

---
+++ Problem/app/src/androidTest/java/au/com/realestate/hometime/MainActivityTest.java
@@ -0,0 +1,20 @@
+package au.com.realestate.hometime;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void refreshClick() {
+    }
+
+    @Test
+    public void clearClick() {
+    }
+}
