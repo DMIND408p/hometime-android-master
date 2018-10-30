fa5e2bd1b8a24abc208d9a9c4a6fa424908c068c
DMIND408P <yyog6@student.monash.edu>
2018-10-30 15:38:52 +1100

    Test case working correctly

--- Problem/app/src/androidTest/java/au/com/realestate/hometime/MainActivityTest.java
+++ Problem/app/src/androidTest/java/au/com/realestate/hometime/MainActivityTest.java
@@ -1,11 +1,34 @@
 package au.com.realestate.hometime;
 
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertNotNull;
 
 public class MainActivityTest {
 
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private MainActivity mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mainActivityActivityTestRule.getActivity();
+    }
+    @Test
+
+    public void testLaunch(){
+        View view = mActivity.findViewById(R.id.northListView);
+        assertNotNull(view);
+
+
+    }
     @Test
     public void onCreate() {
     }
@@ -17,4 +40,12 @@ public class MainActivityTest {
     @Test
     public void clearClick() {
     }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+
+    }
+
+
 }
